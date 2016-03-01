3b8f6d442069cae7a8a4809d789bafdccdee9690
Nmuta Jones <nmuta1@gmail.com>
2016-03-01 11:36:24 -0700

    add method to get request

--- ang_app/app/js/services.js
+++ ang_app/app/js/services.js
@@ -3,12 +3,12 @@ app.service("CatService", function($http){
 
   CatService.getCats = function(){
     //return $http.get("http://localhost:3000/cats");
-    return $http.get("https://frozen-badlands-34577.herokuapp.com/cats");
+    return $http.get("https://frozen-badlands-34577.herokuapp.com/cats", {method: "jsonp"});
   }
 
   CatService.getCat = function(cat_id){
     //return $http.get("http://localhost:3000/cats"+cat_id);
-    return $http.get("https://frozen-badlands-34577.herokuapp.com/cats"+cat_id);
+    return $http.get("https://frozen-badlands-34577.herokuapp.com/cats"+cat_id, {method: "jsonp"});
   }
 
   return CatService;
