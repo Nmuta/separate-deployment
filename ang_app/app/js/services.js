6ac1fbb047c480e4549e3557296ddd8b54362270
Nmuta Jones <nmuta1@gmail.com>
2016-03-01 10:07:11 -0700

    gutting out app for repo

--- ang_app/app/js/services.js
+++ ang_app/app/js/services.js
@@ -1,13 +1,5 @@
 app.service("CatService", function($http){
   var CatService = {};
 
-  CatService.getCats = function(){
-    return $http.get("http://localhost:3000/cats");
-  }
-
-  CatService.getCat = function(cat_id){
-    return $http.get("http://localhost:3000/cats/"+cat_id);
-  }
-
   return CatService;
 });
