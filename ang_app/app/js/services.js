b68b3a4804711119c459a37169d45dda44f4bd9c
Nmuta Jones <nmuta1@gmail.com>
2016-03-01 11:46:38 -0700

    adding private files for student example

--- ang_app/app/js/services.js
+++ ang_app/app/js/services.js
@@ -8,7 +8,7 @@ app.service("CatService", function($http){
 
   CatService.getCat = function(cat_id){
     //return $http.get("http://localhost:3000/cats"+cat_id);
-    return $http.get("https://frozen-badlands-34577.herokuapp.com/cats"+cat_id, {method: "jsonp"});
+    return $http.get("https://frozen-badlands-34577.herokuapp.com/cats/"+cat_id, {method: "jsonp"});
   }
 
   return CatService;
