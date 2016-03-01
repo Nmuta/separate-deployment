bf9683b41224f6de813e00b9bfbc9584572945d6
Nmuta Jones <nmuta1@gmail.com>
2016-03-01 11:22:29 -0700

    view to show query results

--- ang_app/app/js/controllers.js
+++ ang_app/app/js/controllers.js
@@ -1,5 +1,14 @@
 app.controller("MainController", function($scope, $http, CatService){
+  $scope.cats = "the cat gang...";
 
-  $scope.cats = [];
+  CatService.getCats().then(function(payload){
+    console.log("we are getting all Cats");
+    $scope.cat_collection = payload.data;
+  });
+
+  CatService.getCat(1).then(function(single){
+    console.log("we are getting ONE Cat:");
+    $scope.singleCat = single.data;
+  });
 
 });
--- ang_app/app/js/services.js
+++ ang_app/app/js/services.js
@@ -2,11 +2,13 @@ app.service("CatService", function($http){
   var CatService = {};
 
   CatService.getCats = function(){
-    return $http.get("http://localhost:3000/cats");
+    //return $http.get("http://localhost:3000/cats");
+    return $http.get("https://frozen-badlands-34577.herokuapp.com/cats");
   }
 
   CatService.getCat = function(cat_id){
-    return $http.get("http://localhost:3000/cats/"+cat_id);
+    //return $http.get("http://localhost:3000/cats"+cat_id);
+    return $http.get("https://frozen-badlands-34577.herokuapp.com/cats"+cat_id);
   }
 
   return CatService;
